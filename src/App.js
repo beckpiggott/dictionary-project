1b5b70578bf15d13a90cc22ac955e08242209160
Beck Piggott <beck.vipond@gmail.com>
2021-05-15 13:55:08 +0100

    search engine created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
+    
     <div className="App">
+      <div className="container">
+        
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Dictionary Project
         </p>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      
+      <footer className="text-center"><small>Open-source code by Beck Piggott</small></footer>
+    </div>
     </div>
   );
 }
